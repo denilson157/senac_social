757e2859ae335fc2b26ca9b3e9b64a47d54d071a
Denilson <deene67@gmail.com>
2022-04-07 22:13:29 -0300

    criar novo post e integração com api do cloudinary

---
+++ src/components/modal/NewPost.js
@@ -0,0 +1,73 @@
+import React, { useState, useRef } from 'react'
+
+
+const NewPost = ({ isNewPost }) => {
+
+    if (isNewPost) {
+        const element = document.getElementById('newPost')
+        if (!element.classList.contains('show'))
+            new window.bootstrap.Modal(element).show();
+    }
+
+    const [texto, setTexto] = useState('')
+    const image = useRef();
+
+    const uploadImage = async () => {
+        const mockData = new FormData();
+
+        mockData.append('file', image.current.files[0])
+        mockData.append('upload_preset', 'senacgram')
+        mockData.append('cloud_name', 'dpereira')
+
+        const response = await fetch('http://api.cloudinary.com/v1_1/dpereira/image/upload', {
+            method: 'POST',
+            accept: 'applicaton/json',
+            body: mockData
+        })
+
+        const respJson = await response.json();
+
+        return respJson.url
+    }
+
+    const criarPost = async () => {
+
+        const imageURl = await uploadImage()
+
+        const objetoPost = {
+            text: texto,
+            image_id: imageURl
+        }
+
+        console.log(objetoPost)
+
+    }
+
+    return (
+        <div className="modal" id="newPost" tabIndex="-1">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Criar um post</h5>
+                        <button type="button" className="close" >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <textarea rows={7} value={texto} onChange={e => setTexto(e.target.value)} placeholder='Digite um texto' className='form-control my-2'>
+
+                        </textarea>
+
+                        <input type="file" className='form-control my-2' ref={image} />
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary" onClick={criarPost}>Postar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewPost
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../auth';
+import NewPost from '../modal/NewPost';
 
-export default function Navbar(){
+const Navbar = () => {
+
+    const { currentUser } = useContext(UserContext);
+    const [isNewPost, setIsNewPost] = useState(false)
+
     return (
         <nav className='navbar fixed-top navbar-light bg-light'>
             <div className="container">
+
                 <Link to='/' className='navbar-brand'>Senacgram</Link>
-                <ul className='navbar-nav me-auto'>
-                    <li className='nav-item'>
+
+                <ul className='navbar-nav me-auto flex-row space-between'>
+                    <li className='nav-item grow-1'>
                         <Link className='nav-link' to="/explorer">Explorar</Link>
                     </li>
+
                 </ul>
+
+                <ul className='navbar-nav ms-auto flex-row align-items-center'>
+                    {
+                        currentUser &&
+                        <li className='nav-item mx-2'>
+                            <div>
+
+                                <button className='btn' onClick={() => setIsNewPost(old => !old)}>
+                                    New post
+                                    <NewPost isNewPost={isNewPost} />
+                                </button>
+                            </div>
+                        </li>
+                    }
+                    <li className='nav-item'>
+                        {currentUser.name}
+                    </li>
+                </ul>
+
             </div>
-        </nav>);
+        </nav>
+    );
 }
+
+export default Navbar
