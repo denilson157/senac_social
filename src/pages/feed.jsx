7cc7f34afcf2fcd96fc50d2c6ee96e278b0eff00
Denílson <dpereira@actioncargo.com.br>
2022-05-05 07:22:54 -0300

    remoção codigo inutilizado

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Layout from '../components/shared/Layouts';
 import Post from '../components/feed/Post';
-import { useState } from 'react';
 
 import { useQuery } from '@apollo/client';
 
 import { GET_POSTS } from '../graphql/post/query';
-import { useEffect } from 'react';
 
 const FeedPage = () => {
 
-    const { loading, error, data, refetch } = useQuery(GET_POSTS);
+    const { loading, data, refetch } = useQuery(GET_POSTS);
 
     return (
         <Layout>
